2a124cb4e307f67a242075b38fcab75b696305ac
CarterMcClure <124079655+CarterMcClure@users.noreply.github.com>
2023-02-01 21:02:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ var ballSpeed = 5;
 
 var p1 = 0;
 var p2 = 0;
+document.getElementById("p1").innerHTML = p1;
+document.getElementById("p2").innerHTML = p2; 
 
 const leftPaddle = {
   // start in the middle of the game on the left side
